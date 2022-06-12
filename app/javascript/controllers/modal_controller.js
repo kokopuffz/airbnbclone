df5add55fcc0bd673bbaed697c157c890f282b3c
grace <chanheeya@gmail.com>
2022-06-11 21:56:45 -0500

    modal closes when clicked outside

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -2,9 +2,22 @@ import { Controller } from "@hotwired/stimulus"
 import { enter, leave, toggle } from "el-transition"
 
 export default class extends Controller {
-  static targets = ["openUserMenu"]
   connect() {
-    console.log('hello, we are inside the modal controller')
+    enter(document.getElementById('modal-wrapper'));
+    enter(document.getElementById('modal-backdrop'));
+    enter(document.getElementById('modal-panel'));
+
+    document.getElementById('modal-wrapper').addEventListener('click', this.closeModal);
+  
   }
 
+  closeModal(event) {
+    const modalPanelClicked = document.getElementById('modal-panel').contains(event.target);
+
+    if (!modalPanelClicked){
+      leave(document.getElementById('modal-wrapper'));
+      leave(document.getElementById('modal-backdrop'));
+      leave(document.getElementById('modal-panel'));
+    }
+  }
 }
