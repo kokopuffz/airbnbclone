405b314b6a32e8ba53ed33bdabe1866583788afb
grace <chanheeya@gmail.com>
2022-06-19 01:49:59 -0500

    testing files

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -3,6 +3,13 @@
 module Api 
   class UsersController < ApplicationController
     def show
+      user = User.find(params[:id])
+
+      respond_to do |format|
+        format.json do
+          render json: user.to_json, status: :ok
+        end
+      end
     end
   end
 end
