2a1194098fe67f23ea27635c9fccbebe06a643b6
grace <chanheeya@gmail.com>
2022-06-18 21:11:09 -0500

    add api testing

---
+++ spec/requests/api/users_spec.rb
@@ -0,0 +1,19 @@
+require "rails_helper"
+
+Rspec.describe "Api: :Users", type: :request do
+  
+  describe "GET show" do
+    context "user exists" do
+      it "is successful" do
+        
+      end
+    end
+
+    context "user does not exist" do
+      it "is not found" do 
+        
+      end
+    end
+    end
+  end 
+end
